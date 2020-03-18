9716a8b85dad97d1ca420c2c9d15b79562580788
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-03-17 21:25:13 -0300

    Initial commit

---
+++ bin/cdk-study-infra.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkStudyInfraStack } from '../lib/cdk-study-infra-stack';
+
+const app = new cdk.App();
+new CdkStudyInfraStack(app, 'CdkStudyInfraStack');
---
+++ test/cdk-study-infra.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import CdkStudyInfra = require('../lib/cdk-study-infra-stack');
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkStudyInfra.CdkStudyInfraStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
